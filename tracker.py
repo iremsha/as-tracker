071c47e9ff5614d291f94918606e849b1e27d338
IRemsha <meww.mailbox@gmail.com>
2019-04-25 23:32:31 +0500

    add file

---
+++ tracker.py
@@ -0,0 +1,37 @@
+import sys
+from subprocess import Popen, PIPE
+
+import requests
+from prettytable import PrettyTable
+
+
+def main():
+    address = sys.argv[0]
+    adrs = input()
+
+    table = PrettyTable(["Number", "IP", "AS", "CITY", "COUNTRY"])
+    p = Popen(['tracert', "-d", adrs], stdout=PIPE)
+    AS, city, ip, country, counter = '', '', '', '', 0
+    while True:
+        line = p.stdout.readline().decode('cp1256')
+        if 'ms' in line:
+            ip = line.split(' ')[-2]
+            data = requests.get('https://ipinfo.io/{}/json'.format(ip))
+            data_lines = data.text.split('\n')
+            for data in data_lines:
+                if 'org' in data:
+                    AS = data.split(':')[-1]
+                elif 'city' in data:
+                    city = data.split(':')[-1]
+                elif 'country' in data:
+                    country = data.split(':')[-1]
+
+            counter += 1
+            table.add_row([counter, ip, AS, city, country])
+            print(table)
+
+        if not line:
+            break
+
+
+main()
